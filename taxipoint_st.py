b379831f5336340f3b603edc1c223a66bc98b3d1
pyrypp <120693130+pyrypp@users.noreply.github.com>
2024-11-24 15:33:04 +0200

    update layout width

--- taxipoint_st.py
+++ taxipoint_st.py
@@ -1,136 +1,137 @@
-try:
-    import streamlit as st
+import streamlit as st
 
-    import taxiplot
-    # from sqlalchemy import create_engine
-    import os
-    import time
-    import requests
-
-
-    import plotly.io as pio
-    from PIL import Image
-    from io import BytesIO
+import taxiplot
+# from sqlalchemy import create_engine
+import os
+import time
+import requests
 
 
-    # # #
-    os.environ['TZ'] = 'Europe/Helsinki'
-    time.tzset()
-
-    # db_str = st.secrets["db_str"]
-    # sql_engine = create_engine(db_str)
+import plotly.io as pio
+from PIL import Image
+from io import BytesIO
 
 
-    st.markdown(
-            f"""
-    <style>
-        .appview-container .main .block-container{{
-            max-width: {2000}px;
-            padding-left: {2}rem;
-            paddint-right: {2}rem;
-        }}
-    </style>
-    """,
-            unsafe_allow_html=True,
-        )
+# # #
+os.environ['TZ'] = 'Europe/Helsinki'
+time.tzset()
 
-    st.error("Palvelun ylläpito on valitettavasti päättynyt.")
-
-    st.markdown(
-        "<h2 style='text-align: center; color: black;'>Helsinki-Vantaan lentoasema  <br>Taksikysynnän ennuste</h2>", 
-        unsafe_allow_html=True
-    )
+# db_str = st.secrets["db_str"]
+# sql_engine = create_engine(db_str)
 
 
-    # # #
+# st.markdown(
+#         f"""
+# <style>
+#     .appview-container .main .block-container{{
+#         max-width: {2000}px;
+#         padding-left: {2}rem;
+#         paddint-right: {2}rem;
+#     }}
+# </style>
+# """,
+#         unsafe_allow_html=True,
+#     )
+
+st.set_page_config(layout="wide")
+
+st.error("Palvelun ylläpito on valitettavasti päättynyt.")
+
+st.markdown(
+    "<h2 style='text-align: center; color: black;'>Helsinki-Vantaan lentoasema  <br>Taksikysynnän ennuste</h2>", 
+    unsafe_allow_html=True
+)
 
 
-    r_button = st.radio(label="label",options=["KAIKKI", "VAIN MENEVÄ"], label_visibility="hidden", horizontal=True)
-
-    if r_button=="KAIKKI":
-        with st.spinner("Loading..."):
-            # response = requests.get("https://taxipoint-pp.s3.eu-north-1.amazonaws.com/plot_sum.png", stream=True)
-            # im = Image.open(BytesIO(response.content))
-            with Image.open("./plots/plot_sum.png") as im:
-                st.image(im, use_column_width ="always")
-
-    if r_button=="VAIN MENEVÄ":
-        with st.spinner("Loading..."):
-            # response = requests.get("https://taxipoint-pp.s3.eu-north-1.amazonaws.com/plot_sum_me.png", stream=True)
-            # im = Image.open(BytesIO(response.content))
-            with Image.open("./plots/plot_sum_me.png") as im:
-                st.image(im, use_column_width ="always")
+# # #
 
 
+r_button = st.radio(label="label",options=["KAIKKI", "VAIN MENEVÄ"], label_visibility="hidden", horizontal=True)
 
-    st.write("---")
+if r_button=="KAIKKI":
+    with st.spinner("Loading..."):
+        # response = requests.get("https://taxipoint-pp.s3.eu-north-1.amazonaws.com/plot_sum.png", stream=True)
+        # im = Image.open(BytesIO(response.content))
+        with Image.open("./plots/plot_sum.png") as im:
+            st.image(im, use_column_width ="always")
 
-    st.markdown("""
-    Tämä palvelu tarjoaa ennusteen Helsinki-Vantaan lentokentän taksiaseman kysynnästä. Ennusteet perustuvat Taxipointin historiallista dataa hyödyntävään tekoälymalliin. Palvelu on tarkoitettu antamaan yleiskuvaa taksikysynnän vaihteluista eri ajankohtina.
+if r_button=="VAIN MENEVÄ":
+    with st.spinner("Loading..."):
+        # response = requests.get("https://taxipoint-pp.s3.eu-north-1.amazonaws.com/plot_sum_me.png", stream=True)
+        # im = Image.open(BytesIO(response.content))
+        with Image.open("./plots/plot_sum_me.png") as im:
+            st.image(im, use_column_width ="always")
 
-    **Huomio:** Tämä projekti on harrasteprojekti, eikä virallinen palvelu. Ennusteet eivät ole taattuja. Suosittelen käyttämään ennustetta vain suuntaa-antavana tietona.
 
 
-    \- Pyry Pohjanoksa
-    """)
+st.write("---")
 
-    st.write("---")
+st.markdown("""
+Tämä palvelu tarjoaa ennusteen Helsinki-Vantaan lentokentän taksiaseman kysynnästä. Ennusteet perustuvat Taxipointin historiallista dataa hyödyntävään tekoälymalliin. Palvelu on tarkoitettu antamaan yleiskuvaa taksikysynnän vaihteluista eri ajankohtina.
 
-    col1, col2 = st.columns(2)
-    with col1:  
-        with st.form("feedback_form", clear_on_submit=True):
-            st.write("Palautelaatikko")
+**Huomio:** Tämä projekti on harrasteprojekti, eikä virallinen palvelu. Ennusteet eivät ole taattuja. Suosittelen käyttämään ennustetta vain suuntaa-antavana tietona.
 
-            arvosana = st.feedback("stars")
-            if arvosana != None:
-                arvosana += 1
 
-            teksti = st.text_input(label="label", placeholder="Vapaa sana...", max_chars=256, label_visibility="hidden")
+\- Pyry Pohjanoksa
+""")
 
-            submitted = st.form_submit_button("Lähetä")
+st.write("---")
 
-            if submitted:
-                with st.spinner("Odota..."):
-                    # taxiplot.save_to_sql_feedback(arvosana, teksti, sql_engine)
-                    st.write("Palaute lähetetty. Kiitos!")
-    
-    st.write("---")
+col1, col2 = st.columns(2)
+with col1:  
+    with st.form("feedback_form", clear_on_submit=True):
+        st.write("Palautelaatikko")
 
-    st.markdown("""
-        **v1.2.1** (21.8.2024)  
+        arvosana = st.feedback("stars")
+        if arvosana != None:
+            arvosana += 1
 
-        Päivitys:
-        Huippujen laskentalogiikkaa on muutettu vertailukelpoisemmaksi.
-    """)
+        teksti = st.text_input(label="label", placeholder="Vapaa sana...", max_chars=256, label_visibility="hidden")
 
-    st.write("")
+        submitted = st.form_submit_button("Lähetä")
 
-    st.markdown("""
-        **v1.2.0** (19.8.2024)  
+        if submitted:
+            with st.spinner("Odota..."):
+                # taxiplot.save_to_sql_feedback(arvosana, teksti, sql_engine)
+                st.write("Palaute lähetetty. Kiitos!")
 
-        Uusi toiminto:
-        Sovelluksesta löytyy nyt ennuste Menevän taksijonolle erikseen.
-    """)
+st.write("---")
 
-    st.write("")
+st.markdown("""
+    **v1.2.1** (21.8.2024)  
 
-    st.markdown("""
-        **v1.1.0** (16.8.2024)  
+    Päivitys:
+    Huippujen laskentalogiikkaa on muutettu vertailukelpoisemmaksi.
+""")
 
-        Päivitys:
-        Sivusto latautuu huomattavasti nopeammin. Latausaikaa nopeutettu yli 10 sekunnista alle sekuntiin.
-    """)
+st.write("")
 
-    st.write("")
+st.markdown("""
+    **v1.2.0** (19.8.2024)  
 
-    st.markdown("""
-        **v1.0.0** (12.8.2024)  
+    Uusi toiminto:
+    Sovelluksesta löytyy nyt ennuste Menevän taksijonolle erikseen.
+""")
 
-        Sovelluksen ensimmäinen julkinen versio. Kuvaaja näyttää kysynnän edelliseltä 24 tunnilta ja ennusteen seuraavalle 24 tunnille. Ruuhkahuiput on väritetty. Ruuhkahuippujen yhteydessä on luku, joka kertoo, montako asiakasta ruuhkahuipussa on.
+st.write("")
 
-        Sovelluksesta löytyy myös palautelaatikko.
-    """)
+st.markdown("""
+    **v1.1.0** (16.8.2024)  
 
-except:
-    st.error("Palvelun ilmainen käyttöjakso on päättynyt. Kiitos käytöstä!")
+    Päivitys:
+    Sivusto latautuu huomattavasti nopeammin. Latausaikaa nopeutettu yli 10 sekunnista alle sekuntiin.
+""")
+
+st.write("")
+
+st.markdown("""
+    **v1.0.0** (12.8.2024)  
+
+    Sovelluksen ensimmäinen julkinen versio. Kuvaaja näyttää kysynnän edelliseltä 24 tunnilta ja ennusteen seuraavalle 24 tunnille. Ruuhkahuiput on väritetty. Ruuhkahuippujen yhteydessä on luku, joka kertoo, montako asiakasta ruuhkahuipussa on.
+
+    Sovelluksesta löytyy myös palautelaatikko.
+""")
+
+
+# st.error("Palvelun ilmainen käyttöjakso on päättynyt. Kiitos käytöstä!")
 
